542f79ebc355198d918fa8e3939a8043937ff928
azuraopal <muhammadnovalsupriyadi@gmail.com>
2026-08-14 15:56:57 +0700

    style: restructure announcements page logic and standardize message handling

--- src/api/users.js
+++ src/api/users.js
@@ -1,15 +1,9 @@
 import api from './client';
 
 export const usersApi = {
-  getAll: (params = {}) => {
-    const query = new URLSearchParams();
-    if (params.page !== undefined) query.set('page', params.page);
-    if (params.size) query.set('size', params.size);
-    if (params.role) query.set('role', params.role);
-    if (params.keyword) query.set('keyword', params.keyword);
-    return api.get(`/users?${query.toString()}`);
-  },
+  getAll: (params) => api.get('/users', { params }),
   getById: (id) => api.get(`/users/${id}`),
+  create: (data) => api.post('/users', data),
   updateRole: (id, role) => api.patch(`/users/${id}/role`, { role }),
   updateStatus: (id, isActive) => api.patch(`/users/${id}/status`, { isActive }),
   delete: (id) => api.delete(`/users/${id}`),
